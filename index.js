7ec554657413278e4d59ba8622dc164c60cf6d4e
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-13 01:26:15 -0500

    tryingOutCodes

--- index.js
+++ index.js
@@ -12,5 +12,28 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (title){
+    const titles = title.split(" ")
+    let result = ""
+    for (let i=0; i<titles.length; i++) {
+      titles[i] = titles[i][0].toUpperCase() + titles[i].substr(1)
+      result += words[i]+ " "
+
+      return result.slice(0,-1)
+    }
+  })
+}
+
+///////////////////////////////////////////////////////
+const titleCased = () => 
+{
+  return tutorials.map(word => {
+    const words = word.split(" ");
+     let result = "";
+     for (let i = 0; i < words.length; i++) {
+       words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+       result += words[i]+" "                                   };
+     
+     return result.slice(0,-1);
+ });
 }
